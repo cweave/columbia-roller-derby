3d533a3a8c766fa0074b7d549da43a6223a931a9
cweave <cweav3r@gmail.com>
2021-12-10 18:55:04 -0500

    idk

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,61 @@
-const Express = require("express")
-const BodyParser = require("body-parser")
-//const Router = require("./config/routes")
-const app = Express()
-app.use(BodyParser.text({ type: "text/plain" }))
-//Router.routesConfig(app)
+// const proxy = require("http-proxy-middleware")
+
 require("dotenv").config({
-  path: ".env",
+	path: ".env",
 })
+
 module.exports = {
-  siteMetadata: {
-    title: `Columbia Roller Derby`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      // The name of the plugin
-      resolve: "gatsby-source-mongodb",
-      options: {
-        // Name of the database and collection where are books reside
-        dbName: "columbia-roller-derby",
-        collection: "rookies",
-        connectionString: process.env.CRD_DATABASE_NAME,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+	// developMiddleware: app => {
+	// 	app.use(
+	// 		proxy({
+	// 			target: "http://localhost:3000",
+	// 		})
+	// 	)
+	// },
+	siteMetadata: {
+		title: `Columbia Roller Derby`,
+		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		author: `@gatsbyjs`,
+		siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-image`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				// This will impact how browsers show your PWA/website
+				// https://css-tricks.com/meta-theme-color-and-trickery/
+				// theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			// The name of the plugin
+			resolve: "gatsby-source-mongodb",
+			options: {
+				// Name of the database and collection where are books reside
+				dbName: "columbia-roller-derby",
+				collection: "rookies",
+				connectionString: process.env.CRD_DATABASE_NAME,
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 }
--- src/pages/rookie-skaters.js
+++ src/pages/rookie-skaters.js
@@ -12,7 +12,7 @@ const RookieSkaters = props => {
 		<Layout>
 			<Link to="/">Go back to the homepage</Link>
 			<div className="book-container">
-				<DerbyTable data={rookies} />
+				<DerbyTable rookies={rookies} />
 			</div>
 		</Layout>
 	)
@@ -21,26 +21,27 @@ const RookieSkaters = props => {
 export default RookieSkaters
 
 export const query = graphql`
-  {
-    allMongodbColumbiarollerderbyRookies {
-      edges {
-        node {
-          id
-          name
-          derby_name
-          email
-          phone
-          street_address
-          city
-          state
-          county
-          birthday
-          number
-          zip_code
-          member_status
-          yearly_waiver_status
-        }
-      }
-    }
-  }
+  query rookiesQuery {
+	allMongodbColumbiarollerderbyRookies {
+		edges {
+		node {
+			id
+			name
+			derby_name
+			email
+			phone
+			street_address
+			city
+			state
+			county
+			birthday
+			number
+			zip_code
+			member_status
+			yearly_waiver_status
+		}
+		}
+	}
+}
+
 `
