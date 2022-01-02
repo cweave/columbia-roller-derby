4ddd9515d8bdf6b53435b980a26d99bf181c6b73
cweave_ <cweav3r@gmail.com>
2022-01-01 23:55:14 -0500

    more fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,23 @@ import Layout from "../components/layout"
 import NewSkaterForm from "../components/new-skater-form"
 
 const IndexPage = () => {
-	const url =
-		process.env.NODE_ENV === "development" ? process.env.GATSBY_ENV_URL : ""
-	fetch(`${url}/api/`)
-		.then((data) => data.json())
-		.then((data) => {
-			console.log("dattttta", data)
-		})
-	return (
-		<Layout>
-			<div className="book-container">
-				<h1>New Columbia Skaters</h1>
+  const baseUrl = process.env.GATSBY_ENV_URL
+  const url =
+    process.env.NODE_ENV === "development" ? process.env.GATSBY_ENV_URL : ""
+  fetch(`${new URL(baseUrl)}api/`)
+    .then((data) => data.json())
+    .then((data) => {
+      console.log("dattttta", data)
+    })
+  return (
+    <Layout>
+      <div className="book-container">
+        <h1>New Columbia Skaters</h1>
 
-				<NewSkaterForm />
-			</div>
-		</Layout>
-	)
+        <NewSkaterForm />
+      </div>
+    </Layout>
+  )
 }
 
 export default IndexPage
