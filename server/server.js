116fedecafb46f28e6d6817a470b766895d949d4
cweave_ <cweav3r@gmail.com>
2022-01-03 16:55:32 -0500

    more stuff

--- server/server.js
+++ server/server.js
@@ -2,25 +2,41 @@ const express = require("express")
 const cors = require("cors")
 const MongoClient = require("mongodb").MongoClient
 const routes = require("../routes")
+require("dotenv").config()
 
-const uri = process.env.GATSBY_CRD_DATABASE_NAME
-const client = new MongoClient(`${uri}`, { useNewUrlParser: true })
+const uri = `${process.env.GATSBY_CRD_DB_WRITE}`
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+const host =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://columbia-roller-derby.netlify.app/"
 
-client.connect((err) => {
-  if (err) {
-    throw err
-  }
-  const app = express()
-  app.use(
-    cors({
-      origin: "http://localhost:8000",
-    })
-  )
-
-  app.use(express.json())
-  app.use("/api", routes)
-
-  app.listen(3001, () => {
-    console.log("Server has started!")
-  })
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+
+async function run() {
+  try {
+    await client.connect()
+
+    const app = express()
+    console.log(host)
+    app.use(
+      cors({
+        origin: host,
+      })
+    )
+
+    app.use(express.json())
+    app.use("/api", routes)
+
+    app.listen(3001, () => {
+      console.log("Server has started!")
+    })
+  } finally {
+    await client.close()
+  }
+}
+
+run().catch(console.dir)
